3e1b412168fea7de3fd4c6e46154437c5cc902be
olyaatamanjuk <olyaatamanjuk@gmail.com>
2016-04-03 01:58:08 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,28 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
-## creates a special "matrix" object that can cache its inverse
+## This is a pair of function that cache the inverse of matrix
+
+
+## This function is used to set the matrix and or set the inverse matrix
+## the user also can get the matrix that has been set
+## and can get the cached inversed matrix
+
 makeCacheMatrix <- function(x = matrix()) {
-     m <- NULL
-    set <- function(y) {
-        x <<- y
-        m <<- NULL
-     }
-     get <- function() x
-     setsolve <- function(solve) m <<- solve
-     getsolve <- function() m
-     list(set = set, get = get,
-          setsolve = setsolve,
-          getsolve = getsolve)
- }
-## computes the inverse of the special "matrix" 
-cacheSolve <- function(x, ...){
-    m <- x$getsolve()
-    if(!is.null(m)) {
-        message("getting cached data")
-        return(m)
-    }
-    data <- x$get()
-    m <- solve(data, ...)
-    x$setsolve(m)
-    m
+        m <- NULL
+        
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x 
+        
+        setInverse <- function(invmatrix) m <<- invmatrix
+        
+        getInverse <- function() m 
+        
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
+}
+
+
+## This function is used to return the inverse matrix 
+## that has been set up in makeCacheMatrix$setInverse()
+## or return the inverse matrix of matrix 'x' 
+
+cacheSolve <- function(x, ...) {
+        
+        # this code is run when the user set setInverse() in makeCacheMatrix function
+        
+        m <- x$getInverse()
+        
+        #checking if the user set the inverse matrix or not
+        if(!is.null(m)) { 
+                message("getting cached data")
+                return(m)
+        }
+        
+        # this code is run if the user haven't setInverse() in makeCacheMatrix function
+        # and give the inversed matrix value as the return
+        
+        data <- x$get()
+        m <- solve(data)
+        
+        x$setInverse(m) #set the inverse matrix
+        m               #return inversed matrix
 }
